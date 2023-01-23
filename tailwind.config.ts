8e48cdcc3cb05c8a4897908b3c7f1d0855a6ef58
Васильев Александр <wyndace.av@ya.ru>
2023-01-23 13:30:52 +0500

    Добавлены цвета и шрифты

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,24 @@
 import type { Config } from "tailwindcss"
 
-export default <Partial<Config>>{}
+export default <Partial<Config>>{
+  theme: {
+    colors: {
+      "heat-green": "#69B358",
+      "heat-yellow-100": "#f8b006",
+      "heat-yellow-200": "#e5a40a",
+      "heat-orange": "#e94216",
+      "heat-purple": "#665cac",
+      "heat-blue-100": "#459caf",
+      "heat-blue-200": "#60a8de",
+      "heat-gray-100": "#868a8b",
+      "heat-gray-200": "#9d9d9d",
+      "heat-gray-300": "#adadad",
+      "heat-black-100": "#1b2224",
+      "heat-black-200": "#181818",
+      "heat-black-300": "#292929",
+    },
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+    },
+  },
+}
